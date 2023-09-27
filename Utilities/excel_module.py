216d6a51d72eae3f1ac58c65104845fe38ce164e
Shaik Abdul Aulleem <130554283+Shaik-95@users.noreply.github.com>
2023-09-27 17:25:54 +0530

    Update excel_module.py

--- Utilities/excel_module.py
+++ Utilities/excel_module.py
@@ -1,6 +1,5 @@
 import xlrd
 
-
 def read_excel():
 	workbook = xlrd.open_workbook(r"C:\Users\shaik\PycharmProjects\Project_VIA\Excell_files\data1.xlsx")
 	worksheet = workbook.sheet_by_name("positive")
